c55a59a61c2af615227c914b27a7a8159bea16ae
Paul T <paultavares1@gmail.com>
2018-10-28 11:57:57 -0400

    Built bundles with latest

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -1,3 +1,8 @@
+//--------------------------------------------------------------------------------
+// Exports `Icon` with all currently available sources.
+// If wanting to avoid the "bulk", use the `Icon/Base/Icon` class instead
+// add your own custom sources to that.
+//--------------------------------------------------------------------------------
 import Icon from "./base/Icon";
 import {boxicons} from "./source.boxicons";
 import {officeUiFabric} from "./source.office-ui-fabric";
@@ -6,6 +11,7 @@ import {twenmoji} from "./source.twenmoji";
 Icon.sources.boxicons = boxicons;
 Icon.sources.twenmoji = twenmoji;
 Icon.sources["office-ui-fabric"] = officeUiFabric;
+
 
 export { Icon };
 export default Icon;
